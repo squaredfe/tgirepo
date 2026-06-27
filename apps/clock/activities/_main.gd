1c03c9e93c659f5cce18aa7f242ab27b69f71796
FeFe <90733495+squaredfe@users.noreply.github.com>
2026-06-27 20:28:27 -0300

    test2

--- apps/clock/activities/_main.gd
+++ apps/clock/activities/_main.gd
@@ -5,6 +5,7 @@ extends Control
 func _ready() -> void:
 	# Fix ttitlebnar margins
 	$TitleBar.position.y += SystemUIManager.StatusBarMargins
+	$SmoothScrollContainer/Panel/MarginContainer.add_theme_constant_override("margin_top", SystemUIManager.StatusBarMargins + $TitleBar.size.y)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
